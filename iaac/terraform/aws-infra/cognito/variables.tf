311a9e88190485cbb0f1ce3c0d5cbea8404bcd03
Judy Heflin <jaheflin@amazon.com>
2022-09-15 02:16:20 -0700

    Build test 9/15/2022

--- iaac/terraform/aws-infra/cognito/variables.tf
+++
@@ -1,9 +0,0 @@
-variable "cognito_user_pool_name" {
-  description = "Cognito UserPool name"
-  type        = string
-}
-
-variable "aws_route53_zone_id" {
-  description = "Route 53 hosted zone"
-  type        = string
-}
